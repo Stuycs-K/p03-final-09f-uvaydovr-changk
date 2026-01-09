f58c3cfc744ae9561b12c3802debf9b31bc68a1a
Rachel Uvaydov <r.uvaydov18@gmail.com>
2026-01-09 07:29:02 -0500

    Added comments to networking functions about how to change them to work with this project

--- networking.c
+++ networking.c
@@ -14,9 +14,11 @@ int server_setup(){
   hints->ai_flags =AI_PASSIVE;
   getaddrinfo(NULL, "9845", hints, &results); //Server sets node to NULL
 
-  //create the socket
-  int clientd;//store the socket descriptor here
-  clientd=socket(results->ai_family,results->ai_socktype,results->ai_protocol);
+  //create two sockets
+  int p1d;//store the socket descriptor here
+  int p2d;
+  p1d=socket(results->ai_family,results->ai_socktype,results->ai_protocol);
+  p2d=socket(results->ai_family,results->ai_socktype,results->ai_protocol);
 
   //this code should get around the address in use error
   int yes = 1;
@@ -24,13 +26,21 @@ int server_setup(){
   err(sockOpt,"sockopt  error");
 
   //bind the socket to address and port
-  if(bind(clientd,results->ai_addr,results->ai_addrlen)==-1){
+  if(bind(p1d,results->ai_addr,results->ai_addrlen)==-1){
+    printf("%s\n",strerror(errno));
+    exit(1);
+  }
+  if(bind(p2d,results->ai_addr,results->ai_addrlen)==-1){
     printf("%s\n",strerror(errno));
     exit(1);
   }
 
   //set socket to listen state
-  if(listen(clientd,1)==-1){ // 1?
+  if(listen(p1d,1)==-1){
+    printf("%s\n",strerror(errno));
+    exit(1);
+  }
+  if(listen(p1d,2)==-1){
     printf("%s\n",strerror(errno));
     exit(1);
   }
@@ -38,15 +48,15 @@ int server_setup(){
   //free the structs used by getaddrinfo
   free(hints);
   freeaddrinfo(results);
-
-  return clientd;
+// setup returns sockets for both players? or run server_setup() twice?
+  return p1d;
 }
 
 /*Accept a connection from a client
  *return the socket descriptor for the new socket connected to the client
  *blocks until connection is made.
  */
-int server_tcp_handshake(int listen_socket){
+int server_tcp_handshake(int listen_socket){ //server does this for each player
     int client_socket;
     struct sockaddr_storage add;
     socklen_t size=sizeof(add);
@@ -64,7 +74,7 @@ int server_tcp_handshake(int listen_socket){
 /*Connect to the server
  *return the to_server socket descriptor
  *blocks until connection is made.*/
-int client_tcp_handshake(char * server_address){
+int client_tcp_handshake(char * server_address){ //both players do this with server
   //getaddrinfo
   struct addrinfo * hints;
   struct addrinfo * results;
