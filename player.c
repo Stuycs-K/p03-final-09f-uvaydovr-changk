c78f36c789d99ce8e3a289620e29ab9660081063
Katherine <kat517rose@gmail.com>
2026-01-09 14:44:39 -0500

    merging player logics

--- player.c
+++ player.c
@@ -1,15 +1,25 @@
 #include "networking.h"
 
 
-void player1Logic(){ // parameter: socket?
-  printf("Connecting to Player 2...\n");
-  //TCP Handshake
+void playerLogic(int server_socket, int playerTurn){ 
+  printf("Connecting to Player %d...\n", playerTurn);
 
+  char token;
+  int oppositePlayer;
   char* board=malloc(42*sizeof(char));
   for(int i=0;i<42;i++){
     *(board+i)='_';
   }
-  printf("Your token is X\n\n");
+
+  if (playerTurn == 1) {
+      printf("Your token is X\n\n");
+      token = 'X';
+      oppositePlayer = 2;
+  } elseif (playerTurn == 2) {
+      printf("Your token is O\n\n");
+      token = 'O';
+      oppositePlayer = 1;
+  }
 
   while(checkBoard(board)==0){  //function in main.c
     printBoard(board); //main.c
@@ -27,58 +37,15 @@ void player1Logic(){ // parameter: socket?
       break;
     }
 
-    printf("Player 2 is taking their turn...\n\n");
-    //player 2 turn stuff
+    printf("Player %d is taking their turn...\n\n", oppositePlayer);
 
     if(checkBoard(board)==2){
       printBoard(board);
-      printf("Player 2 wins!\n");
+      printf("Player d wins!\n", oppositePlayer);
       break;
     }
   }
 }
-
-
-void player2Logic(){
- printf("Connecting to Player 1...\n");
- //TCP Handshake
-
- char* board=malloc(42*sizeof(char));
- for(int i=0;i<42;i++){
-   *(board+i)='_';
- }
- printf("Your token is O\n\n");
-
- while(checkBoard(board)==0){  //function in main.c
-   printf("Player 1 is taking their turn...\n\n");
-   //player 1 turn stuff
-
-   if(checkBoard(board)==1){
-     printBoard(board);
-     printf("Player 1 wins!\n");
-     break;
-   }
-
-   printBoard(board); //main.c
-   int col=0;
-   printf("Which column do you want to put a piece in?\n");
-   scanf("%d",&col);
-   while(updateBoard(board,col)==-1){ //main.c
-     printf("Column %d is already filled. Please enter a column with space for a new piece:\n",col);
-     scanf("%d",&col);
-   }
-
-   if(checkBoard(board)==2){
-     printBoard(board);
-     printf("You win!\n");
-     break;
-   }
-
- }
-
- return;
-}
-
 
 int main(int argc, char *argv[]) {
   char *IP = "127.0.0.1";
@@ -86,6 +53,9 @@ int main(int argc, char *argv[]) {
   int sd = client_tcp_handshake(IP);
   if (sd < 0) { printf("connect failed\n"); return 1; }
   printf("connected\n");
+
+  player1Logic(sd);
+
   close(sd);
   return 0;
 }
